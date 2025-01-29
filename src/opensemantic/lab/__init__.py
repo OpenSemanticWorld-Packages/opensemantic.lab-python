03c6080f106ae0f3f1dbe7fc82aaa4eecccc4f30
Lukas Gold <lukas.gold@isc.fraunhofer.de>
2025-01-29 17:08:06 +0100

    Setup

--- src/opensemantic/lab/__init__.py
+++ src/opensemantic/lab/__init__.py
@@ -1,10 +1,10 @@
-import sys
+# Necessary to allow a submodule to be defined in a separate repository and use this
+#  package as its namespace package:
+from pkgutil import extend_path
 
-if sys.version_info[:2] >= (3, 8):
-    # TODO: Import directly (no need for conditional) when `python_requires = >= 3.8`
-    from importlib.metadata import PackageNotFoundError, version  # pragma: no cover
-else:
-    from importlib_metadata import PackageNotFoundError, version  # pragma: no cover
+__path__ = extend_path(__path__, __name__)
+
+from importlib.metadata import PackageNotFoundError, version  # pragma: no cover
 
 try:
     # Change here if project is renamed and does not equal the package name
